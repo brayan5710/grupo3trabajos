16562ea891f593485079ec4ae613ae415a952400
triplec21 <kev.3ccc@gmail.com>
2016-05-18 12:36:12 -0400

    Add files via upload

--- assets/js/jquery/func.js
+++ assets/js/jquery/func.js
@@ -24,7 +24,35 @@ $ ( document ).ready(function(){
 	
 
 		event.preventDefault();
-	})
+	});
+
+	$('#saluda').click(function()
+	{
+		var nombre = $('#entrada').val();
+		saludo(nombre);
+	});
+
+
+	$('#calcula').click(function(){
+		var num1 = parseInt($('#valorA').val(),10);
+		var num2 = parseInt($('#valorB').val(),10);
+		operacion(num1,num2);
+	});
+
+
+	
 
 });
+
+
+function saludo(persona){
+	alert('hola '+persona);
+}
+
+function operacion(a,b){
+	var resultado=a+b;
+	alert('La Suma de '+ a +' más '+ b +' es igual a '+resultado);
+}
+
+
 
